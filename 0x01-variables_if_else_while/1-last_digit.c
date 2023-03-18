573ab974332cc47e8a9aedd4d6a2a39909f033be
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-03-17 17:44:12 -0700

    remove and is

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ int main(void)
 	}
 	else if (digit == 0)
         {
-        printf("Last digit of %d is %d and is and is 0\n", n, digit);
+        printf("Last digit of %d is %d and is 0\n", n, digit);
         }
         else if (digit < 6 && digit != 0)
         {
-        printf("Last digit of %d is %d and is and is less than 6 and not 0\n", n, digit);
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
         }
         return (0);
 }
