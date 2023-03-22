c7b1769c2c9e702eb354a697b261fb541d2e3f4d
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-03-21 17:53:19 -0700

    print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that print the alphabets 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+	int row, ch;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_puchar('\n');
+	}
+}
