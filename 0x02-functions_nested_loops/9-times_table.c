c39dfe93f6db4a021bdf10776e20ce88ae68e224
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-03-21 19:31:26 -0700

    fff

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-/***/
+/**
+ * times_table - print the 9 times table
+*/
 
 
 void times_table(void)
 {
-
 	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
@@ -22,7 +23,7 @@ void times_table(void)
 			_putchar(' ');
 		else
 			_putchar((prod / 10) + 48);
-			_Putchar((prod % 10) + 48);
+		_Putchar((prod % 10) + 48);
 	}
 		_putchar('\n');
 	}
