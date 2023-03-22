3ce60dbe7e5322ac048ac2679e1d98b4aed0771a
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-03-21 17:45:40 -0700

    1s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - uses _putchar function to print the alphabet from a to z
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
