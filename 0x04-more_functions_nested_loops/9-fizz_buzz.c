0b079bdaba8987bfcc03d8431bfd2596fc17e6b5
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-03-24 14:24:28 -0700

    ss

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print numbers from 0 - 100
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+		else if (num % 3)
+			printf("Fizz");
+		else if (num % 5)
+			printf("Buzz");
+		else
+			printf("%d", num);
+
+	if (num != 100)
+		printf(" ");
+	else
+		printf("\n");
+	}
+}
