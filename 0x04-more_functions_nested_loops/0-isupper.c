0647aaa31696359d32ba306ea5ffbd4f695645b8
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-03-23 18:46:41 -0700

    betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,13 @@
 /**
  * _isupper - ckeck if the c is upper
  * @c: input for alphabet
- * Return: 1 if uppercase, 0 if lower
+ * Return: 1 if uppercase
+ * 0 if lower
 */
-
 int _isupper(int c)
 {
-	if (c >= 65 && c <=90)
-		{
-			return (1);
-		}
+	if (c >= 65 && c <= 90)
+		return (1);
 	else
 		return (0);
 }
