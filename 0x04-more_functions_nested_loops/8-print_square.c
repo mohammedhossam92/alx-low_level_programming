6777764cfee84334f6aeb94fdbd6830ee476c134
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-03-24 13:50:15 -0700

    sss

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_square - print square
+ * @size: the size of the square
+ * Return: Always 0
+*/
+void print_square(int size)
+{
+	int row, col;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (col = 1; col <= size; col++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
