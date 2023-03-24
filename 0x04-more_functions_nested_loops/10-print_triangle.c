25568b7abd7fa5dcdd6f41871aa7ceb686e57359
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-03-24 15:10:48 -0700

    aa

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle
+ * @size: integer size
+*/
+void print_triangle(int size)
+{
+	int row, ht;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (ht = 0; ht <= size; ht++)
+		{
+			for (row = 1; row <= size; row++)
+			{
+				if ((ht + row) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+	}
+			_putchar('\n');
+		}
+}
+}
