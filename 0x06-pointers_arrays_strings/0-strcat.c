7a2e49f10da8e2cdf1e96d57d58cbc7305502dda
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-03-30 17:04:13 -0700

    betty

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,7 @@ char *_strcat(char *dest, char *src)
 	c = 0;
 	while (dest[c])
 		c++;
-	for (c2 = 0; src[c2]; c2++)
-			dest[c++] = src[c2];
+	for (c2 = 0; src[c2] ; c2++)
+		dest[c++] = src[c2];
 	return (dest);
 }
