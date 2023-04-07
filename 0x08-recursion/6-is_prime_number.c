aa37992fa4f4137867b9f9048305ef0b4309e196
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-04-07 08:58:42 -0700

    b

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,25 +5,25 @@
  * @n: int
  * Return: 0 or 1
 */
-int if_prime(int n, int o);
+int c_prime(int n, int o);
 
 int is_prime_number(int n)
 {
-	return (if_prime(n, 2));
+	return (c_prime(n, 2));
 }
 
 /**
- * if_prime - function to check all numbers < n if they can divided by it
+ * c_prime - function to check all numbers < n if they can divided by it
  * @n: int
  * @o: int
  * Return: int
 */
-int if_prime(int n, int o)
+int c_prime(int n, int o)
 {
 	if (o >= n && n > 1)
 		return (1);
 	else if (n % o == 0 || n <= 1)
 		return (0);
 	else
-		return (if_prime(n, o + 1));
+		return (c_prime(n, o + 1));
 }
