f58251b280b89f7e5ec02dffba9f0318bcfe8eea
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-04-06 17:38:19 -0700

    dd

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,15 @@
 #include "main.h"
-/***/
+/**
+* _puts_recursion - funtion prints string
+* @s: pointer
+*/
 void _puts_recursion(char *s)
 {
-	
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_put_recursion(s + 1);
+	}	
 }
