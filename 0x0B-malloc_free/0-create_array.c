1e251ddfe8cc9d8b8786afdef84219ef2fa7e9b0
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-04-11 15:29:13 -0700

    git push

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,18 @@
 #include "main.h"
-/***/
+/**
+ *create_array - function
+ *@size: size of array
+ *@c: character to initialize
+ *Return: return i
+*/
 
 char *create_array(unsigned int size, char c)
 {
-	
+	char *i = malloc(size);
+
+	if (size == 0 || i == 0)
+		return (0);
+	while (size--)
+		i[size] = c;
+	return (i);
 }
