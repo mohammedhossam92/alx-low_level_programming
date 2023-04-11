c83558bc2b235c20fd78aecd8aecb08a9851d024
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-04-10 17:28:22 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function to print file name
+ * @argc: int
+ * @argv: int
+ * Return: return 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
