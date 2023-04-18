c581d4478889630bd6b8c0482c328ecaf7e39576
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-04-17 18:27:03 -0700

    git push

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - struct
+ *@name: dog's name
+ *@owner: dog's owner
+ *@age: dog's age
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+
+};
+
+#endif
