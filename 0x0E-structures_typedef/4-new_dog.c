c9bd2e08ad673e1df02ea1b2eb3ea1dc31d92e6d
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-04-18 16:24:27 -0700

    git push

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,6 +18,24 @@ int _strlen(const char *str)
 }
 
 /**
+ * _strcopy - function
+ * @src: string to copy from
+ *@dest: destination
+ *Return: dest
+*/
+
+char *_strcopy(char *dest, char *src)
+	{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+
+	}
+
+/**
  * new_dog - function
  *
  *@name: dog's name
