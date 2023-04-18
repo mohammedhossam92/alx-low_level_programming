d4c0e0ddba9c8419972ee7b11c1f55d2f84f42ae
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-04-18 16:19:54 -0700

    git push

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,26 +18,6 @@ int _strlen(const char *str)
 }
 
 /**
- * _strcopy - function
- * @src: string to copy from
- *@dest: destination
- *Return: dest
-*/
-
-char *_strcopy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; src[i]; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-
-	return (dest);
-
-}
-
-
-/**
  * new_dog - function
  *
  *@name: dog's name
@@ -56,21 +36,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	dog = (dog_t *) malloc(sizeof(dog_t));
-
 	if (dog == NULL)
 		return (NULL);
 
 	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
-
 	if ((*dog).name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 
-	dog->name = _strcopy(dog->name, name);
+	dog->name = name;
 	dog->age = age;
-	dog->owner = _strcopy(dog->owner, owner);
+	dog->owner = owner;
 
 	return (dog);
 }
