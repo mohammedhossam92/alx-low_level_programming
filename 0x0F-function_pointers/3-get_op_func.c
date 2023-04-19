3d70c3ca29fb3218815c4c8d5f6b754fdfe926a1
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-04-19 16:59:48 -0700

    git push

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+
+/**
+ *op_add - function to add
+ * @a: first int
+ * @b: second int
+ *Return: the sum
+*/
+
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - function to substract
+ *@a: first integer
+ *@b: second int
+ *Return: sub
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_div - function to divide
+ *@a: first int
+ *@b: second int
+ *Return: return result
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ *op_mod - function to return the remainder
+ *@a: first int
+ *@b: second int
+ *Return: return the result
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
