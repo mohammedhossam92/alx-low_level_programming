d85eed636627373d52479f126747f7583023d42a
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-04-27 19:29:59 -0700

    suspect error

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ *add_node_end - function to add a node at the end of a single list
+ *@head: add of a pointer to the head node
+ *@str: string field of the node
+ *
+ *Return: s
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *end_node = malloc(sizeof(list_t));
+	list_t *node = *head;
+
+	if (!head || !end_node)
+		return (NULL);
+	else if (str)
+	{
+		end_node->str = strdup(str);
+		if (!end_node->str)
+		{
+			free(end_node);
+			return (NULL);
+		}
+		end_node->len = _strlen(end_node->str);
+	}
+	else if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = end_node;
+	}
+	else
+		*head = end_node;
+	return (end_node);
+
+}
