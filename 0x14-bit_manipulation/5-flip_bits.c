9b1837bec3abefd38712a478e2349a8abae192c8
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-05-04 20:10:36 -0700

    cc

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*flip_bits - function to return the num of bits that will neeed to be flipped
+*
+*@n: first number
+*
+*@m: second number
+*
+*Return: num of bits to flip
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	/*declaing*/
+	unsigned long int x = n ^ m;
+	unsigned int count = 0;
+
+	/*code*/
+	while (x)
+	{
+		if (x & 1ul)
+			count++;
+		x = x >> 1;
+	}
+	return (count);
+}
