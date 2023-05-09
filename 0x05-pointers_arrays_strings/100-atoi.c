4a6ef58ddfbd17b2313cdfcff5b7f8c21dcfaa20
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-05-08 20:46:31 -0700

    test

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _atoi - function
+ * @s:pointer
+ * Return: return void
+*/
+int _atoi(char *s)
+{
+	return ();
+}
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
 *_strspn - function
-*Return: return void
 *@s: pointer
 *@accept: pointer
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	return ();
+	
 }
