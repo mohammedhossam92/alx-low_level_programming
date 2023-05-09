9cf4f8353a5c5a374371d254fb765761fff27875
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-05-08 22:02:59 -0700

    change file to fd

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,7 +11,7 @@
 #define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOREAD "Error: can't read from file %s\n"
 #define	ERR_NOWRITE "Error: can't write to %s\n"
-#define ERR_NOCLOSE "Error: can't close file %d\n"
+#define ERR_NOCLOSE "Error: can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 int _putchar(char c);
